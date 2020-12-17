c767998ba0b07e4af4bf3542a6bec40ab96f60ba
Tommy B <taz88308@gmail.com>
2020-12-17 17:15:17 +0000

    add helper functions file

---
+++ helper_functions.py
@@ -0,0 +1,53 @@
+import requests
+import json
+import datetime
+
+from replit import db
+
+def get_quote():
+    response = requests.get("https://zenquotes.io/api/random")
+    json_data = json.loads(response.text)
+    quote = json_data[0]['q'] + " -" + json_data[0]['a']
+    return (quote)
+
+
+def get_playlist_name():
+    today = datetime.datetime.now()
+    playlist_name = today.strftime("%b") + today.strftime("%Y")
+    return (playlist_name)
+
+
+def update_playlist_db(name):
+    if "playlists" in db.keys():
+        playlist = db["playlists"]
+        playlist.append(name)
+    else:
+        db["playlists"] = [name]
+
+
+def update_fuck_words(message):
+    if "fucks" in db.keys():
+        fucks = db["fucks"]
+        fucks.append(message)
+    else:
+        db["fucks"] = [message]
+
+
+def delete_fuck_words(idx):
+    fucks = db["fuck_words"]
+    if len(fucks) > idx:
+        del fucks[idx]
+        db["fucks"] = fucks
+
+
+def botify_helper(content, arg):
+    botify_str = ""
+    if arg == "playlist":
+        botify_str = "$botify add " + content + " $to " + get_playlist_name(
+        ) + "\n"
+    elif arg == "queue":
+        botify_str = "$botify queue " + content + "\n"
+    elif arg == "create":
+        botify_str = "$botify create " + get_playlist_name() + "\n"
+    return (botify_str)
+
--- main.py
+++ main.py
@@ -1,55 +1,18 @@
 import discord
 import os
-import requests
-import json
 import time
-import datetime
+
+from keep_alive import keep_alive
+from helper_functions import get_quote
+from helper_functions import get_playlist_name
+from helper_functions import update_playlist_db
+from helper_functions import update_fuck_words
+from helper_functions import delete_fuck_words
+from helper_functions import botify_helper
 
 from replit import db
-from keep_alive import keep_alive
 
 client = discord.Client()
-
-def get_quote():
-  response = requests.get("https://zenquotes.io/api/random")
-  json_data = json.loads(response.text)
-  quote = json_data[0]['q'] + " -" + json_data[0]['a']
-  return(quote)
-
-def get_playlist_name():
-  today = datetime.datetime.now()
-  playlist_name = today.strftime("%b") + today.strftime("%Y")
-  return(playlist_name)
-
-def update_playlist_db(name):
-  if "playlists" in db.keys():
-    playlist = db["playlists"]
-    playlist.append(name)
-  else:
-    db["playlists"] = [name]
-
-def update_fuck_words(message):
-  if "fucks" in db.keys():
-    fucks = db["fucks"]
-    fucks.append(message)
-  else: 
-    db["fucks"] = [message]
-
-def delete_fuck_words(idx):
-  fucks = db["fuck_words"]
-  if len(fucks) > idx:
-    del fucks[idx]
-    db["fucks"] = fucks
-
-def botify_helper(content, arg):
-  botify_str = ""
-  if arg == "playlist":
-    botify_str = "$botify add " + content + " $to " + get_playlist_name() + "\n"
-  elif arg == "queue":
-    botify_str = "$botify queue " + content + "\n"
-  elif arg == "create":
-    botify_str = "$botify create " + get_playlist_name() + "\n"
-  return(botify_str)
 
 fuck_words = ["fuck", "shit", "crap", "damn", "dammit", "titty", "ass", "fucking", "shitty", "cunt"]
 
@@ -67,7 +30,8 @@ async def on_message(message):
   if message.author == client.user:
     return
 
-  if message.author.name.lower() == "botify":
+  if message.author.bot.lower() == "true":
+    #ignore bots
     return
 
   playlists = playlist_list
@@ -78,8 +42,6 @@ async def on_message(message):
   channel_name = message.channel.name
 
   if channel_name == "monthly-playlist-drop":
-    await message.channel.send(botify_helper(message.content, "create"))
-    time.sleep(10)
     if get_playlist_name() not in playlists:
       update_playlist_db(get_playlist_name())
       await message.channel.send(botify_helper(message.content, "create"))
